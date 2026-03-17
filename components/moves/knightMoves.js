2050a650339e5e1299dc5b7a7af54ee8d3c5c323
Nigam Subedi <nigam333nigam@gmail.com>
2026-03-17 20:42:52 +0545

    fixed knight movement

--- components/moves/knightMoves.js
+++ components/moves/knightMoves.js
@@ -1,25 +1,35 @@
-export const getKnightMoves = (board, row, col) =>{
-    const possible = []
+export const getKnightMoves = (board, row, col) => {
 
-    const KnightMoves = [
-        [-2, -1], [-2,1],
-        [-1, -2], [-1, 2],
-        [1, -2], [1, 2],
-        [2, -1], [2, 1]
-    ];
-    KnightMoves.forEach(([rOffset, cOffset]) =>{
-        const newRow = row + rOffset;
-        const newCol = col + cOffset;
-        
-        //check Board Boundaries
-        if(
-            newRow >= 0 &&
-            newRow < 8 &&
-            newCol >= 0 &&
-            newCol < 8
-        ){
-            possible.push([newRow, newCol])
-        }
-    })
-    return possible;
-}
+  const possible = [];
+  const color = board[row][col][0]; // 'w' or 'b'
+
+  const knightMoves = [
+    [-2, -1], [-2, 1],
+    [-1, -2], [-1, 2],
+    [1, -2], [1, 2],
+    [2, -1], [2, 1]
+  ];
+
+  knightMoves.forEach(([rOffset, cOffset]) => {
+
+    const newRow = row + rOffset;
+    const newCol = col + cOffset;
+
+    // check boundaries
+    if (
+      newRow >= 0 &&
+      newRow < 8 &&
+      newCol >= 0 &&
+      newCol < 8
+    ) {
+      const target = board[newRow][newCol];
+
+      // allow if empty OR enemy piece
+      if (!target || target[0] !== color) {
+        possible.push([newRow, newCol]);
+      }
+    }
+  });
+
+  return possible;
+};
